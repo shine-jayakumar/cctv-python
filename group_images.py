124f35cb5d5e7cda06f141646c58152e6df9b8f5
shine-jayakumar <shinejayakumar@yahoo.com>
2023-09-05 18:12:06 +0530

    Added GROUP_IMAGES_INTERVAL constant

--- group_images.py
+++ group_images.py
@@ -1,11 +1,12 @@
 import time
 from modules.helpers import group_images
+from constants.constants import GROUP_IMAGES_INTERVAL
 
 
 st = time.time()
 while True:
     en = time.time()
-    if en - st >= 2:
+    if en - st >= GROUP_IMAGES_INTERVAL:
         group_images()
         st = en
         
